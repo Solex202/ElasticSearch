396018fbf154988b1fe03b017543bceb06b74ea0
Solex202 <onwukalotachukwu210@gmail.com>
2024-02-18 14:57:03 +0100

    first commit

--- src/main/java/com/example/demo/config/ElasticSearchConfiguration.java
+++ src/main/java/com/example/demo/config/ElasticSearchConfiguration.java
@@ -1,15 +1,20 @@
-//package com.example.demo.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class ElasticSearchConfiguration{
+package com.example.demo.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+
+@Configuration
+public class ElasticSearchConfiguration{
 //    @Bean
 //    public RestHighLevelClient elasticsearchClient() {
 //        ClientConfiguration clientConfiguration = ClientConfiguration.builder()
 //                .connectedTo("localhost:9200")
 //                .build();
 //
-//        return RestClients.create(clientConfiguration).rest();
+//        RestHighLevelClient.class;
+//
+////        return RestClients.create(clientConfiguration).rest();
 //    }
-//}
+}
